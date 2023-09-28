c79f76d46060072a19216c353598dc2313cce21e
camiloruiz <camiloruizvidal@gmail.com>
2023-09-28 18:26:43 -0500

    other way

--- back/database/migrations/1695502313371_add_new_columns_to_schema.js
+++ back/database/migrations/1695502313371_add_new_columns_to_schema.js
@@ -4,134 +4,204 @@ const Schema = use('Schema')
 
 class AddNewColumnsToSchema extends Schema {
   up () {
-    this.table('tbl_registros_personales', (table) => {
-      if (!table.hasColumn('razon_social_ips')) {
-        table.string('razon_social_ips').nullable()
+    this.table('tbl_registros_personales',async (table) => {
+      const tableName = 'tbl_registros_personales'
+      const columns = await Database.schema.raw(`PRAGMA table_info(${tableName})`)
+      const existingColumns = columns.map((column) => column.name)
+
+      // Agregar campos uno por uno si no existen
+      if (!existingColumns.includes('razon_social_ips')) {
+        this.alterTable(tableName, (table) => {
+          table.string('razon_social_ips').nullable()
+        })
       }
 
-      if (!table.hasColumn('lengua_propia')) {
-        table.string('lengua_propia').nullable()
+      if (!existingColumns.includes('lengua_propia')) {
+        this.alterTable(tableName, (table) => {
+          table.string('lengua_propia').nullable()
+        })
       }
 
-      if (!table.hasColumn('tipo_ocupacion')) {
-        table.string('tipo_ocupacion').nullable()
+      if (!existingColumns.includes('tipo_ocupacion')) {
+        this.alterTable(tableName, (table) => {
+          table.string('tipo_ocupacion').nullable()
+        })
       }
 
-      if (!table.hasColumn('salud_mental')) {
-        table.string('salud_mental').nullable()
+      if (!existingColumns.includes('salud_mental')) {
+        this.alterTable(tableName, (table) => {
+          table.string('salud_mental').nullable()
+        })
       }
 
-      if (!table.hasColumn('conocimiento_salud')) {
-        table.string('conocimiento_salud').nullable()
+      if (!existingColumns.includes('conocimiento_salud')) {
+        this.alterTable(tableName, (table) => {
+          table.string('conocimiento_salud').nullable()
+        })
       }
 
-      if (!table.hasColumn('poblacion_vulnerable')) {
-        table.string('poblacion_vulnerable').nullable()
+      if (!existingColumns.includes('poblacion_vulnerable')) {
+        this.alterTable(tableName, (table) => {
+          table.string('poblacion_vulnerable').nullable()
+        })
       }
 
-      if (!table.hasColumn('tipo_discapacidad')) {
-        table.string('tipo_discapacidad').nullable()
+      if (!existingColumns.includes('tipo_discapacidad')) {
+        this.alterTable(tableName, (table) => {
+          table.string('tipo_discapacidad').nullable()
+        })
       }
 
-      if (!table.hasColumn('tiene_cuidador')) {
-        table.boolean('tiene_cuidador').nullable()
+      if (!existingColumns.includes('tiene_cuidador')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('tiene_cuidador').nullable()
+        })
       }
 
-      if (!table.hasColumn('descapacidad_registro_localizacion_caracteri')) {
-        table.string('descapacidad_registro_localizacion_caracteri').nullable()
+      if (!existingColumns.includes('descapacidad_registro_localizacion_caracteri')) {
+        this.alterTable(tableName, (table) => {
+          table.string('descapacidad_registro_localizacion_caracteri').nullable()
+        })
       }
 
-      if (!table.hasColumn('presento_tos_expectoracion')) {
-        table.boolean('presento_tos_expectoracion').nullable()
+      if (!existingColumns.includes('presento_tos_expectoracion')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('presento_tos_expectoracion').nullable()
+        })
       }
 
-      if (!table.hasColumn('fuma')) {
-        table.boolean('fuma').nullable()
+      if (!existingColumns.includes('fuma')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('fuma').nullable()
+        })
       }
 
-      if (!table.hasColumn('hace_cuanto')) {
-        table.string('hace_cuanto').nullable()
+      if (!existingColumns.includes('hace_cuanto')) {
+        this.alterTable(tableName, (table) => {
+          table.string('hace_cuanto').nullable()
+        })
       }
 
-      if (!table.hasColumn('cuantos_cigarrillos_diarios')) {
-        table.integer('cuantos_cigarrillos_diarios').nullable()
+      if (!existingColumns.includes('cuantos_cigarrillos_diarios')) {
+        this.alterTable(tableName, (table) => {
+          table.integer('cuantos_cigarrillos_diarios').nullable()
+        })
       }
 
-      if (!table.hasColumn('expocicion_quimicos')) {
-        table.boolean('expocicion_quimicos').nullable()
+      if (!existingColumns.includes('expocicion_quimicos')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('expocicion_quimicos').nullable()
+        })
       }
 
-      if (!table.hasColumn('expocicion_convulsivos')) {
-        table.boolean('expocicion_convulsivos').nullable()
+      if (!existingColumns.includes('expocicion_convulsivos')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('expocicion_convulsivos').nullable()
+        })
       }
 
-      if (!table.hasColumn('religion')) {
-        table.string('religion').nullable()
+      if (!existingColumns.includes('religion')) {
+        this.alterTable(tableName, (table) => {
+          table.string('religion').nullable()
+        })
       }
 
-      if (!table.hasColumn('consumo_alcohol')) {
-        table.boolean('consumo_alcohol').nullable()
+      if (!existingColumns.includes('consumo_alcohol')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('consumo_alcohol').nullable()
+        })
       }
 
-      if (!table.hasColumn('frecuancia_consumo_alcohol')) {
-        table.string('frecuancia_consumo_alcohol').nullable()
+      if (!existingColumns.includes('frecuancia_consumo_alcohol')) {
+        this.alterTable(tableName, (table) => {
+          table.string('frecuancia_consumo_alcohol').nullable()
+        })
       }
 
-      if (!table.hasColumn('hace_cuanto_alcohol')) {
-        table.string('hace_cuanto_alcohol').nullable()
+      if (!existingColumns.includes('hace_cuanto_alcohol')) {
+        this.alterTable(tableName, (table) => {
+          table.string('hace_cuanto_alcohol').nullable()
+        })
       }
 
-      if (!table.hasColumn('consumo_sustan_psicoactivas')) {
-        table.boolean('consumo_sustan_psicoactivas').nullable()
+      if (!existingColumns.includes('consumo_sustan_psicoactivas')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('consumo_sustan_psicoactivas').nullable()
+        })
       }
 
-      if (!table.hasColumn('sustan_psicoactivas')) {
-        table.string('sustan_psicoactivas').nullable()
+      if (!existingColumns.includes('sustan_psicoactivas')) {
+        this.alterTable(tableName, (table) => {
+          table.string('sustan_psicoactivas').nullable()
+        })
       }
 
-      if (!table.hasColumn('frecuensia_sustan_psicoactivas')) {
-        table.string('frecuensia_sustan_psicoactivas').nullable()
+      if (!existingColumns.includes('frecuensia_sustan_psicoactivas')) {
+        this.alterTable(tableName, (table) => {
+          table.string('frecuensia_sustan_psicoactivas').nullable()
+        })
       }
 
-      if (!table.hasColumn('hace_cuanto_sustan_psicoactivas')) {
-        table.string('hace_cuanto_sustan_psicoactivas').nullable()
+      if (!existingColumns.includes('hace_cuanto_sustan_psicoactivas')) {
+        this.alterTable(tableName, (table) => {
+          table.string('hace_cuanto_sustan_psicoactivas').nullable()
+        })
       }
 
-      if (!table.hasColumn('planificacion_familiar')) {
-        table.boolean('planificacion_familiar').nullable()
+      if (!existingColumns.includes('planificacion_familiar')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('planificacion_familiar').nullable()
+        })
       }
 
-      if (!table.hasColumn('fecha_ultima_citologia')) {
-        table.date('fecha_ultima_citologia').nullable()
+      if (!existingColumns.includes('fecha_ultima_citologia')) {
+        this.alterTable(tableName, (table) => {
+          table.date('fecha_ultima_citologia').nullable()
+        })
       }
 
-      if (!table.hasColumn('fecha_ultima_mestruacion')) {
-        table.date('fecha_ultima_mestruacion').nullable()
+      if (!existingColumns.includes('fecha_ultima_mestruacion')) {
+        this.alterTable(tableName, (table) => {
+          table.date('fecha_ultima_mestruacion').nullable()
+        })
       }
 
-      if (!table.hasColumn('sufre_enf_cronica')) {
-        table.boolean('sufre_enf_cronica').nullable()
+      if (!existingColumns.includes('sufre_enf_cronica')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('sufre_enf_cronica').nullable()
+        })
       }
 
-      if (!table.hasColumn('patologias')) {
-        table.string('patologias').nullable()
+      if (!existingColumns.includes('patologias')) {
+        this.alterTable(tableName, (table) => {
+          table.string('patologias').nullable()
+        })
       }
 
-      if (!table.hasColumn('alimientacion_lactancia')) {
-        table.string('alimientacion_lactancia').nullable()
+      if (!existingColumns.includes('alimientacion_lactancia')) {
+        this.alterTable(tableName, (table) => {
+          table.string('alimientacion_lactancia').nullable()
+        })
       }
 
-      if (!table.hasColumn('frecu_sepillado')) {
-        table.boolean('frecu_sepillado').nullable()
+      if (!existingColumns.includes('frecu_sepillado')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('frecu_sepillado').nullable()
+        })
       }
 
-      if (!table.hasColumn('utiliza_crema_dental')) {
-        table.boolean('utiliza_crema_dental').nullable()
+      if (!existingColumns.includes('utiliza_crema_dental')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('utiliza_crema_dental').nullable()
+        })
       }
 
-      if (!table.hasColumn('ingesta_crema_dental')) {
-        table.boolean('ingesta_crema_dental').nullable()
+      if (!existingColumns.includes('ingesta_crema_dental')) {
+        this.alterTable(tableName, (table) => {
+          table.boolean('ingesta_crema_dental').nullable()
+        })
       }
+
     })
   }
 
