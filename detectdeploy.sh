a36eb581b2a80549093986023cff0061a13ce38d
camiloruiz <camiloruizvidal@gmail.com>
2023-10-05 10:18:50 -0500

    change information

--- detectdeploy.sh
+++ detectdeploy.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-commit_count=$(git rev-list --count main)
+git fetch
 
-# Comprobar si el número de commits ha cambiado desde la última vez
-if [ -f last_commit_count.txt ]; then
-  last_count=$(cat last_commit_count.txt)
-  if [ "$commit_count" -ne "$last_count" ]; then
-    echo "La rama main ha tenido cambios."
-  else
-    echo "La rama main no ha tenido cambios desde la última vez."
-  fi
+if [ "$(git rev-parse HEAD)" != "$(git rev-parse origin/main)" ]; then
+  echo "La rama main ha tenido cambios en el repositorio remoto."
+else
+  echo "La rama main no ha tenido cambios en el repositorio remoto."
 fi
-
-echo "$commit_count" > last_commit_count.txt
